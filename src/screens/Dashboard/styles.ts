23d0c00b5cff691d345c5c107d860b336c1de42b
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-13 14:56:06 -0300

    Add global style

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,10 +1,15 @@
+import { ProgressViewIOSComponent } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #999;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.title};
+`;
